f44d6ffbaf5559c3af676344491b2666a912882f
Garrison <Garrison@Garrisons-MacBook-Pro.local>
2011-06-01 22:25:04 -0500

    Main Menu - Add tabs' view controllers, delete template view controllers and xibs

---
+++ Youpon/ProfileMainViewController.h
@@ -0,0 +1,16 @@
+//
+//  ProfileMainViewController.h
+//  Youpon
+//
+//  Created by Garrison on 6/1/11.
+//  Copyright 2011 Garrison Prinslow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ProfileMainViewController : UIViewController {
+    
+}
+
+@end
---
+++ Youpon/SettingsMainViewController.h
@@ -0,0 +1,16 @@
+//
+//  SettingsMainViewController.h
+//  Youpon
+//
+//  Created by Garrison on 6/1/11.
+//  Copyright 2011 Garrison Prinslow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SettingsMainViewController : UIViewController {
+    
+}
+
+@end
